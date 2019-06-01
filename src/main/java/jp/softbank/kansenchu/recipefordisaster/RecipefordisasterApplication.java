fe84de5ffefeb81faef45750a08e90b0f459ba29
Wilson Choo <kansen.chu@g.softbank.co.jp>
2019-06-01 11:12:49 +0900

    initial commit

---
+++ src/main/java/jp/softbank/kansenchu/recipefordisaster/RecipefordisasterApplication.java
@@ -0,0 +1,13 @@
+package jp.softbank.kansenchu.recipefordisaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipefordisasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipefordisasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/softbank/kansenchu/recipefordisaster/RecipefordisasterApplicationTests.java
@@ -0,0 +1,16 @@
+package jp.softbank.kansenchu.recipefordisaster;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecipefordisasterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
