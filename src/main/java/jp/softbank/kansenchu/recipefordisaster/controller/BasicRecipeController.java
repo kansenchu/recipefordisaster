60a59598f30dbc7694e66701803020bb7fe3dfed
Wilson Choo <kansen.chu@g.softbank.co.jp>
2019-06-02 21:11:02 +0900

    controller exception handler

--- src/main/java/jp/softbank/kansenchu/recipefordisaster/controller/BasicRecipeController.java
+++ src/main/java/jp/softbank/kansenchu/recipefordisaster/controller/BasicRecipeController.java
@@ -1,24 +1,31 @@
 package jp.softbank.kansenchu.recipefordisaster.controller;
 
+import com.fasterxml.jackson.annotation.JsonView;
+
 import java.util.List;
 
+import jp.softbank.kansenchu.recipefordisaster.dto.ErrorResponse;
+import jp.softbank.kansenchu.recipefordisaster.dto.MultiRecipeResponse;
+import jp.softbank.kansenchu.recipefordisaster.dto.RecipeDto;
+import jp.softbank.kansenchu.recipefordisaster.dto.SuccessResponse;
+import jp.softbank.kansenchu.recipefordisaster.dto.views.ResponseViews;
+import jp.softbank.kansenchu.recipefordisaster.exception.InvalidRecipeException;
+import jp.softbank.kansenchu.recipefordisaster.exception.RecipeNotFoundException;
+import jp.softbank.kansenchu.recipefordisaster.service.RecipeService;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.fasterxml.jackson.annotation.JsonView;
-
-import jp.softbank.kansenchu.recipefordisaster.dto.MultiRecipeResponse;
-import jp.softbank.kansenchu.recipefordisaster.dto.RecipeDto;
-import jp.softbank.kansenchu.recipefordisaster.dto.SuccessResponse;
-import jp.softbank.kansenchu.recipefordisaster.dto.views.ResponseViews;
-import jp.softbank.kansenchu.recipefordisaster.service.RecipeService;
-import lombok.RequiredArgsConstructor;
 
 /**
  * レシピの各リクエストを受付するクラス.
@@ -79,6 +86,31 @@ public class BasicRecipeController implements RecipeController {
   public SuccessResponse deleteRecipe(@PathVariable int id) {
     recipeService.deleteRecipe(id);
     return new SuccessResponse(SuccessResponse.Message.DELETED);
+  }
+  
+  /**
+   * レシピが見つかれない時対応するハンドラー.
+   * @param ex もらったエクセプション
+   * @return 決定のエラーメッセージ
+   */
+  @JsonView(ResponseViews.MessageOnly.class)
+  @ExceptionHandler(RecipeNotFoundException.class)
+  @ResponseStatus(HttpStatus.OK)
+  public ErrorResponse recipeNotFoundHandler(RecipeNotFoundException ex) {
+    return new ErrorResponse(ErrorResponse.Message.NOT_FOUND);
+  }
+
+  @ExceptionHandler(InvalidRecipeException.class)
+  @ResponseStatus(HttpStatus.OK)
+  public ErrorResponse invalidRecipeHandler(InvalidRecipeException ex) {
+    return new ErrorResponse(ErrorResponse.Message.CREATION_FAILED, ex.getMessage());
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  @ResponseStatus(HttpStatus.OK)
+  public ErrorResponse invalidRecipeHandler(Exception ex) {
+    System.out.println(ex.getCause().getMessage());
+    return new ErrorResponse(ErrorResponse.Message.CREATION_FAILED, ex.getMessage());
   }
 
 }
---
+++ src/main/java/jp/softbank/kansenchu/recipefordisaster/dto/ErrorResponse.java
@@ -0,0 +1,42 @@
+package jp.softbank.kansenchu.recipefordisaster.dto;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.annotation.JsonView;
+
+import jp.softbank.kansenchu.recipefordisaster.dto.views.ResponseViews;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * エラーのレスポンス用POJOクラス.
+ */
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+  public enum Message {
+    NOT_FOUND("No Recipe found"),
+    CREATION_FAILED("Recipe creation failed!");
+
+    private String message;
+    
+    private Message(String message) {
+      this.message = message;
+    }
+
+    @JsonValue
+    public String getMessage() {
+      return message;
+    }
+  }
+
+  /** リクエストに返信メッセージ. */
+  @JsonView(ResponseViews.MessageOnly.class)
+  final Message message;
+
+  /** 欠けてるパラメータのString. */
+  String required;
+
+}
+
